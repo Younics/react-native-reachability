7a03e2fcc1aa605f31456ddfc737c4165b7cdf71
Filip Hoško <filip.hosko@younics.com>
2018-06-14 19:43:36 +0200

    Add listeners for Android, cummulative
    Also refactored settings for loading the library into example project, now it should perform more consistently

--- index.js
+++ index.js
@@ -1,16 +1,48 @@
 // @flow
 
-import { NativeModules } from 'react-native'
+import React, { Component } from 'react'
+import { DeviceEventEmitter, NativeModules } from 'react-native'
 
 const { RNReachability } = NativeModules
 
-export const isReachable = (timeout: Number = 5000): Promise =>
+export const getReachabilityAsync = (timeout: Number = 5000): Promise =>
   RNReachability.isReachable(timeout)
     .then(result => !!result) // iOS returns 1 or 0 here so we convert the value to boolean
     .catch(error => {
       throw error
     })
 
-export default {
-  isReachable
+class Network extends Component {
+  _handleError = message => {
+    console.error('Error while fetching network state: ', message)
+  }
+
+  _handleReachabilityChange = isReachable => {
+    if (typeof this.props.onReachabilityChange === 'function') {
+      this.props.onReachabilityChange(isReachable)
+    }
+  }
+
+  componentDidMount () {
+    RNReachability.startListener()
+    DeviceEventEmitter.addListener(
+      'com.younics.reachability:onReachabilityChange',
+      this._handleReachabilityChange
+    )
+
+    DeviceEventEmitter.addListener(
+      'com.younics.reachability:onError',
+      this._handleError
+    )
+  }
+
+  componentWillUnmount () {
+    RNReachability.stopListener()
+  }
+
+  render () {
+    return null
+  }
 }
+
+export default Network
