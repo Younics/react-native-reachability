17c28b6b401262db6cd2f7730c256c52c5d508bd
Filip Hoško <filip.hosko@younics.com>
2018-11-21 15:14:21 +0100

    refactor: remove warning for isReachable

--- src/index.js
+++ src/index.js
@@ -1,26 +1,15 @@
 /* @flow */
 
-import { Component } from "react";
-import { NativeModules } from "react-native";
+import { Component } from "react"
+import { NativeModules } from "react-native"
 
-export { Network } from "./Network";
+export { Network } from "./Network"
 
-const { RNReachability } = NativeModules;
+const { RNReachability } = NativeModules
 
-export const isReachableAsync = (
-  timeout?: number = 5000
-): Promise<boolean, Error> => RNReachability.isReachable(timeout);
+export const isReachable = (timeout?: number = 5000): Promise<boolean, Error> =>
+  RNReachability.isReachable(timeout)
 
-/* NOTE: for backward compatibilty - will be removed in future versions */
-const isReachableWithWarning = (timeout?: number) => {
-  console.warn(
-    "Network object and its methods are deprecated. Please use Network component for listening to reachability changes or getReachabilityAsync to get actual reachability status."
-  );
-  return isReachableAsync(timeout);
-};
-
-export const isReachable = isReachableWithWarning;
 export default {
   isReachable
-};
-/* <<< */
+}
